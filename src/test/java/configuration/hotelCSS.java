dce5d170033b7add3fcba39b56ea561f0a2beb67
Michał <104276409+Mikael182@users.noreply.github.com>
2022-09-18 15:31:05 +0200

    Elements

--- src/test/java/configuration/hotelCSS.java
+++ src/test/java/configuration/hotelCSS.java
@@ -6,24 +6,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class hotelXpath {
+public class hotelCSS {
     public static void main(String[] args) {
-
         System.setProperty("webdriver.chrome.driver", "src/main/resources/Drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://hotel-testlab.coderslab.pl/en/");
         driver.findElement(By.className("user_login")).click();
-        driver.findElement(By.className("is_required")).sendKeys("test@com.pl");
-        driver.findElement(By.className("icon-user")).submit();
+        driver.findElement(By.id("email")).sendKeys("test@com.pl");
+        driver.findElement(By.id("passwd")).sendKeys("Test01");
+        driver.findElement(By.id("SubmitLogin")).click();
 
-        driver.findElement(By.xpath("//input[@id='customer_firstname']")).sendKeys("Michał");
-        driver.findElement(By.xpath("//input[@id='customer_lastname']")).sendKeys("Tester");
-        driver.findElement(By.xpath("//input[@id='email']"));
-        driver.findElement(By.xpath("//input[@id='passwd']")).sendKeys("Test01");
-
-        driver.findElement(By.xpath("//button[@id='submitAccount']")).submit();
+        driver.findElement(By.cssSelector("i.icon-building"));
+        driver.findElement(By.cssSelector("i.icon-user"));
         driver.quit();
     }
 }
